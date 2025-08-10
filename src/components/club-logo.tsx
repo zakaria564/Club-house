9b6d22f97d5d778bde66b568fc6721d0121714bf
Zakaria Boukhari <taxizak81@gmail.com>
2025-08-10 08:34:46 +0000

    je veux ajouter logo à mon club

---
+++ src/components/club-logo.tsx
@@ -0,0 +1,21 @@
+
+"use client";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+interface ClubLogoProps {
+    className?: string;
+}
+
+export function ClubLogo({ className }: ClubLogoProps) {
+  return (
+    <Image
+        src="https://placehold.co/100x100.png"
+        width={40}
+        height={40}
+        alt="Club CAOS 2011 Logo"
+        className={cn("text-primary", className)}
+        data-ai-hint="club logo"
+     />
+  );
+}
