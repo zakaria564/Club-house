172dbb6edc201c6652bfbf5b2e4249b2d8706854
Zakaria Boukhari <taxizak81@gmail.com>
2025-08-10 18:28:40 +0000

    pour les trois logos

--- src/components/club-logo.tsx
+++ src/components/club-logo.tsx
@@ -9,21 +9,21 @@ interface ClubLogoProps {
 
 export function ClubLogo({ className }: ClubLogoProps) {
   return (
-    <div className="flex items-center justify-center w-full gap-4">
+    <div className="flex items-center justify-center w-full gap-8">
         <Image
             src="https://liguefootcasa.ma/wp-content/uploads/2020/09/logo-ligue-du-casablanca.png"
             width={50}
             height={50}
             alt="Ligue du grand Casablanca de football"
-            className={cn("print:block", className)}
+            className={cn("print:block h-auto", className)}
             data-ai-hint="league logo"
         />
         <Image
             src="https://image.noelshack.com/fichiers/2025/32/7/1754814584-whatsapp-image-2025-02-02-03-31-09-1c4bc2b3.jpg"
-            width={64}
-            height={64}
+            width={50}
+            height={50}
             alt="Club CAOS 2011 Logo"
-            className={cn("text-primary", className)}
+            className={cn("text-primary h-auto", className)}
             data-ai-hint="club logo"
         />
          <Image
@@ -31,7 +31,7 @@ export function ClubLogo({ className }: ClubLogoProps) {
             width={50}
             height={50}
             alt="Fédération Royale Marocaine de Football"
-            className={cn("print:block", className)}
+            className={cn("print:block h-auto", className)}
             data-ai-hint="federation logo"
         />
     </div>
