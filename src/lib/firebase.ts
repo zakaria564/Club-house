7d4c983ca440b4d9371376ae2828ebc1f2e3df7c
Zakaria Boukhari <taxizak81@gmail.com>
2025-08-15 00:05:37 +0000

    ça prend tros de temps et elle ne s'arrête pas de tourner pour le téléve

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -18,7 +18,7 @@ const firebaseConfig: FirebaseOptions = {
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
-const storage = getStorage(app);
+const storage = getStorage(app, firebaseConfig.storageBucket);
 const auth = getAuth(app);
 
 export { app, db, storage, auth };
