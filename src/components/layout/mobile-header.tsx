862ccd7b8e3acb7e22f3d334b53b67c464436e83
Zakaria Boukhari <taxizak81@gmail.com>
2025-08-15 02:34:39 +0000

    le bouton de menu il n'est pas visible lui aussi doit etre en blanc

--- src/components/layout/mobile-header.tsx
+++ src/components/layout/mobile-header.tsx
@@ -6,7 +6,7 @@ import { SidebarTrigger } from "../ui/sidebar";
 export function MobileHeader() {
   return (
     <header className="md:hidden flex items-center justify-between px-4 border-b fixed top-0 left-0 right-0 bg-background z-50 h-16">
-        <SidebarTrigger />
+        <SidebarTrigger className="dark:text-white" />
         <ClubLogo className="h-8 w-auto" />
     </header>
   );
