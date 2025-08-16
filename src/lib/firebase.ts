2e692fafbd727138d9a7c30cf673b1df1af4ce7f
Zakaria Boukhari <taxizak81@gmail.com>
2025-08-16 01:24:14 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp, type FirebaseOptions } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -7,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration is now hardcoded
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  "projectId": "club-house-t50v6",
+  "appId": "1:230928221464:web:017e8c5f758cfca03d8a6d",
+  "storageBucket": "club-house-t50v6.firebasestorage.app",
+  "apiKey": "AIzaSyCifokPPqenjxnZstWn3cy9xiK6U_mywPE",
+  "authDomain": "club-house-t50v6.firebaseapp.com",
+  "messagingSenderId": "230928221464"
 };
 
 // Initialize Firebase
