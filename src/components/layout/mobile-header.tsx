ea6992ba5c19e097cac1a06326518393122de9c3
Zakaria Boukhari <taxizak81@gmail.com>
2025-08-17 00:33:04 +0000

    et supprimer le bouton concernant le menu latéral

--- src/components/layout/mobile-header.tsx
+++ src/components/layout/mobile-header.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { ClubLogo } from "../club-logo";
-import { SidebarTrigger } from "../ui/sidebar";
 
 export function MobileHeader() {
   return (
-    <header className="md:hidden flex items-center justify-between px-4 border-b fixed top-0 left-0 right-0 bg-background z-50 h-16">
-        <SidebarTrigger className="dark:text-white" />
+    <header className="md:hidden flex items-center justify-center px-4 border-b fixed top-0 left-0 right-0 bg-background z-50 h-16">
         <ClubLogo className="h-8 w-auto" />
     </header>
   );
