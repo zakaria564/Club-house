b9ec921722260d5b69141e192d234a0e30ee28ff
Zakaria Boukhari <taxizak81@gmail.com>
2025-08-18 00:07:08 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SidebarContent cl

--- src/components/layout/mobile-header.tsx
+++ src/components/layout/mobile-header.tsx
@@ -6,14 +6,9 @@ import { SidebarTrigger } from "../ui/sidebar";
 
 export function MobileHeader() {
   return (
-    <header className="lg:hidden grid grid-cols-3 items-center px-4 border-b fixed top-0 left-0 right-0 bg-background z-50 h-16">
-        <div className="flex justify-start">
-            <SidebarTrigger />
-        </div>
-        <div />
-        <div className="flex justify-end">
-            <ClubLogo className="h-8 w-auto" />
-        </div>
+    <header className="lg:hidden flex items-center justify-between px-4 border-b fixed top-0 left-0 right-0 bg-background z-50 h-16">
+        <SidebarTrigger />
+        <ClubLogo className="h-8 w-auto" />
     </header>
   );
 }
